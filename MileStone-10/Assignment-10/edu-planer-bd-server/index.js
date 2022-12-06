2dd7d8c37d9ca602bdd6e41d7e737d40c92b5778
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-12-06 20:09:32 +0600

    added courses data and varcel deploy

--- MileStone-10/Assignment-10/edu-planer-bd-server/index.js
+++ MileStone-10/Assignment-10/edu-planer-bd-server/index.js
@@ -5,10 +5,23 @@ const port = process.env.PORT || 4000
 
 app.use(cors());
 
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
     res.send('edu planer bd server side is running')
 })
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(course => course.id === id)
+    res.send(course);
+})
+
+
 
 app.listen(port, () => {
     console.log(`edu planer bd running on post ${port}`);
